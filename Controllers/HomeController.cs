8a56b35d34708083a8884a75d62135e8376f75d9
= <=>
2018-10-01 12:56:40 +0200

    Edit Some things

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using EssentialCSharp.Models;
+
 namespace EssentialCSharp.Controllers{
     public class HomeController : Controller{
         public ViewResult Index(){
-            return View(new string[]{"Mohamed","Salem","Rashaad"});
+           List<string> results = new List<string>();
+            foreach (Product product in Product.GetProducts())
+            {
+                string Name = product?.Name ?? "<No Name>";
+                decimal? Price = product?.Price ?? 0;
+                string RelatedName = product?.Related?.Name ?? "<Not Exist>";
+                results.Add(string.Format("Name : {0} , Price : {1} , Related Name : {2}",Name,Price,RelatedName ));
+            }
+            return View(results);
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -2,14 +2,16 @@
 namespace EssentialCSharp.Models{
     public class Product{
         public string Name { get; set; }
+        public string Category { get; set; } = "";
         public decimal? Price { get; set; }
+        public Product Related{get;set;}
         public static Product[] GetProducts(){
             
             Product AllVit = new Product(){Name ="AllVit",Price = 520};
-            Product Phose = new Product(){Name = "Phose" , Price = 90};
-            Product MycoB = new Product() { Name="MycoB" ,Price = 495};
-            
-            return new Product[]{AllVit,Phose,MycoB,null};
+            Product Phose = new Product(){Name = "Phose" , Price = 90,Related = AllVit};
+            Product MycoB = new Product(){ Name="MycoB" ,Price = 495,Related = Phose};
+            Product VitE = new Product(){ Name="VitE",Related = MycoB};
+            return new Product[]{AllVit,Phose,MycoB,VitE,null};
         }
 
     }
