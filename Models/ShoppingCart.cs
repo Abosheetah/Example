7d71527f2d1dad808b0d41e97881c7e0464ea24e
= <=>
2018-10-02 10:30:02 +0200

    =Add shoppingCart class

---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+namespace EssentialCSharp.Models{
+    public class ShoppingCart {
+        public IEnumerable<Product> Products{set;get;}
+    }
+}
--- ReadMe.md
+++ ReadMe.md
@@ -1,8 +1,8 @@
 # Some Guidances
 ### 1- This project only for training.
 ### 2 - This project By using c# and .net core framework.
-
 '''
 if(true){
     //message
-}'''
+}
+'''
